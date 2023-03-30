0ab6037fe6e3ce7c93527b7a60c78d9568204846
Lucifer <tim.noelmans@outlook.com>
2023-03-30 02:14:14 +0200

    Add database class & config

---
+++ Classes/Db.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database {
+    private static $PDO;
+
+    public static function getInstance()
+    {
+        if (self::$PDO != null) {
+            return self::$PDO;
+        } else {
+            $config = parse_ini_file("config/config.ini");
+            self::$PDO = new PDO('mysql:host=' . $config['db_host'] . ';dbname=' . $config['db_name'], $config['db_user'], $config['db_password']);
+            
+            return self::$PDO;
+        }
+    }
+}
