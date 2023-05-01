158369499916b224fae79bf8a42d5810edec64fc
Lucifer <tim.noelmans@outlook.com>
2023-05-01 02:40:25 +0200

    fix(aside): links to other files

--- partials/aside.inc.php
+++ partials/aside.inc.php
@@ -1,6 +1,6 @@
 <aside>
-    <a class="button primary-btn-white" href="tools/add-prompt">
-        <img src="./assets/images/site/plus-icon.svg" alt="Icon of a plus">
+    <a class="button primary-btn-white" href="<?php echo __ROOT__; ?>tools/add-prompt">
+        <img src="<?php echo __ROOT__; ?>/assets/images/site/plus-icon.svg" alt="Icon of a plus">
         Add a prompt
     </a>
     <h3>Your library</h3>
