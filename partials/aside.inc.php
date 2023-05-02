6fb52cae2abbfc026469ac192b8c4b6ede060e1b
zegher <zegherb@hotmail.com>
2023-05-02 11:28:58 +0200

    side nav: spacing en font update

--- partials/aside.inc.php
+++ partials/aside.inc.php
@@ -3,13 +3,13 @@
         <img src="<?php echo __ROOT__; ?>/assets/images/site/plus-icon.svg" alt="Icon of a plus">
         Add a prompt
     </a>
-    <h2>Browse</h2>
+    <h2 class="titles">Browse</h2>
     <ul class="nav-ul">
         <li><a href="#">Hot</a></li>
         <li><a href="#">New</a></li>
         <li><a href="#">Top</a></li>
     </ul>
-    <h2>Models</h2>
+    <h2 class="titles">Models</h2>
     <ul class="nav-ul">
         <li><a href="#">ChatGPT</a></li>
         <li><a href="#">MidJourney</a></li>
@@ -17,7 +17,7 @@
         <li><a href="#">Open Diffusion</a></li>
         <li><a href="#">DALL-E</a></li>
     </ul>
-    <h2>Your Library</h2>
+    <h2 class="titles">Your Library</h2>
     <ul class="nav-ul">
         <li><a href="#">Liked</a></li>
         <li><a href="#">Bought</a></li>
@@ -26,25 +26,3 @@
     </ul>
 </aside>
 <div id="aside-placeholder"></div>
-
-<!-- BROWSE
-
-Featured
-Hot
-New
-Top
-------------
-MODELS
-
-ChatGPT
-Midjourney
-Stable diffusion
-Open diffusion
-DALL-E
------------
-TOP CATEGORIES
-
-Anime
-Photography
-Fashion
-Concept Art -->
