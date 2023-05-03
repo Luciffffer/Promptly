fced29e58021010e3baa656c1e606360b3111d2b
Lucifer <tim.noelmans@outlook.com>
2023-05-03 03:32:47 +0200

    feat(add_prompt): image preview done

--- tools/add-prompt.php
+++ tools/add-prompt.php
@@ -24,116 +24,112 @@ $models = Prompt::getAllModels();
     <?php include_once(__DIR__ . "/../partials/nav.inc.php"); ?>
     <main>
         <?php include_once(__DIR__ . "/../partials/aside.inc.php"); ?>
-        <div>
-            <label for="prompt-header-image">
-                <header id="prompt-header">
-                    <div class="hidden"></div>
-                    <i></i>
-                </header>
+        <form action="" method="POST">
+            <label id="prompt-header" for="prompt-header-image">
+                <div class="hidden"></div>
+                <i></i>
             </label>
-            <input type="file" name="prompt-header-image" id="prompt-header-image" accept=".jpg, .jpeg, .png, .webp" class="hidden">
+            <input data-type="image" type="file" name="header-image" id="prompt-header-image" accept=".jpg, .jpeg, .png, .webp" class="hidden">
             <script src="../assets/js/prompt-header-parallax.js" defer></script>
             <div style="padding: 0 3rem">
                 <div class="center" id="single-prompt-grid">
                     <div>
-                        <form action="" method="POST">
-                            <section id="single-prompt-top">
-                                <p id="single-prompt-top-left">
-                                    <span><span id="word-count">0</span> words</span>
-                                </p>
-                                <div>
-                                    <select name="model" id="model">
-                                        <?php foreach($models as $model) : ?>
-                                            <option value="<?php echo $model['id']; ?>">
-                                                <?php echo $model['name']; ?>
-                                            </option>
-                                        <?php endforeach; ?>
-                                    </select>
+                        <section id="single-prompt-top">
+                            <p id="single-prompt-top-left">
+                                <span><span id="word-count">0</span> words</span>
+                            </p>
+                            <div>
+                                <select name="model" id="model">
+                                    <?php foreach($models as $model) : ?>
+                                        <option value="<?php echo $model['id']; ?>">
+                                            <?php echo $model['name']; ?>
+                                        </option>
+                                    <?php endforeach; ?>
+                                </select>
 
-                                    <select name="model-version" id="model-version">
-                                        <?php foreach(json_decode($models[0]['versions']) as $version) : ?>
-                                            <option value="<?php echo $version; ?>">
-                                                <?php echo $version; ?>
-                                            </option>
-                                        <?php endforeach ?>
-                                    </select>
-                                    <script>
-                                        document.querySelector("#model").onchange = () => {
-                                            const formData = new FormData();
-                                            formData.append("modelId", document.querySelector("#model").value);
-
-                                            fetch("../ajax/get-model-versions.ajax.php", {
-                                                method: "POST",
-                                                body: formData
-                                            })
-                                                .then(response => response.json())
-                                                .then(json => {
-                                                    document.querySelector("#model-version").innerHTML = '';
-
-                                                    json['body'].forEach(version => {
-                                                        const option = document.createElement("option");
-                                                        option.value = version;
-                                                        option.innerHTML = version;
-                                                        document.querySelector("#model-version").appendChild(option);
-                                                    })
-                                                })
-                                                .catch(err => {
-                                                    console.error(err);
-                                                })
-                                        }
-                                    </script>
-                                </div>
-                            </section>
-                            <section>
-                                <div style="position: relative; margin-bottom: 1rem">
-                                    <input id="title-input" type="text" name="title" placeholder="Add a Title...">
-                                    <hr id="title-input-front-hr">
-                                    <hr id="title-input-bg-hr">
-                                </div>
-                                <div class="form-part">
-                                    <fieldset>
-                                        <legend>Tags</legend>
-                                        <textarea class="grey-textarea" name="tags" id="tags" cols="30" rows="1" placeholder="Add keywords that describe your prompt"></textarea>
-                                    </fieldset>
-                                    <small>Separate by a comma or space.</small> 
-                                </div>
-                                <div class="form-part">
-                                    <fieldset>
-                                        <legend>Description</legend>
-                                        <textarea class="grey-textarea" name="description" id="description" placeholder="Add a comprehensive description" cols="30" rows="10"></textarea>
-                                    </fieldset>
-                                </div>   
-                            </section>
-                            <section id="prompt-author-info">
-                                <p class="grey">Created: now :P</p>
-                                <a class="white-a profile-a" href="../profile?id=<?php echo $_SESSION['userId']; ?>">
-                                    <span class="grey">By:</span>
-                                    <figure style="background-image: url(../<?php echo $_SESSION['profile-pic'] ?>);"></figure>
-                                    <span><?php echo htmlspecialchars($_SESSION['username']); ?></span>
-                                </a>
-                            </section>
-                            <section id="prompt-section">
-                                <h2>The prompt:</h2>
-                                <textarea class="grey-textarea" name="prompt" id="prompt" cols="30" rows="5"></textarea>
+                                <select name="model-version" id="model-version">
+                                    <?php foreach(json_decode($models[0]['versions']) as $version) : ?>
+                                        <option value="<?php echo $version; ?>">
+                                            <?php echo $version; ?>
+                                        </option>
+                                    <?php endforeach ?>
+                                </select>
                                 <script>
-                                    document.querySelector("#prompt").addEventListener('keyup', e => {
-                                        const wordCount = document.querySelector("#prompt").value.match(/\S+/g) === null ? 0 : document.querySelector("#prompt").value.match(/\S+/g).length
-                                        
-                                        document.querySelector("#word-count").innerHTML = wordCount
-                                    })
+                                    document.querySelector("#model").onchange = () => {
+                                        const formData = new FormData();
+                                        formData.append("modelId", document.querySelector("#model").value);
+
+                                        fetch("../ajax/get-model-versions.ajax.php", {
+                                            method: "POST",
+                                            body: formData
+                                        })
+                                            .then(response => response.json())
+                                            .then(json => {
+                                                document.querySelector("#model-version").innerHTML = '';
+
+                                                json['body'].forEach(version => {
+                                                    const option = document.createElement("option");
+                                                    option.value = version;
+                                                    option.innerHTML = version;
+                                                    document.querySelector("#model-version").appendChild(option);
+                                                })
+                                            })
+                                            .catch(err => {
+                                                console.error(err);
+                                            })
+                                    }
                                 </script>
-                            </section>
-                        </form>
+                            </div>
+                        </section>
+                        <section>
+                            <div style="position: relative; margin-bottom: 1rem">
+                                <input id="title-input" type="text" name="title" placeholder="Add a Title...">
+                                <hr id="title-input-front-hr">
+                                <hr id="title-input-bg-hr">
+                            </div>
+                            <div class="form-part">
+                                <fieldset>
+                                    <legend>Tags</legend>
+                                    <textarea class="grey-textarea" name="tags" id="tags" cols="30" rows="1" placeholder="Add keywords that describe your prompt"></textarea>
+                                </fieldset>
+                                <small>Separate by a comma or space.</small> 
+                            </div>
+                            <div class="form-part">
+                                <fieldset>
+                                    <legend>Description</legend>
+                                    <textarea class="grey-textarea" name="description" id="description" placeholder="Add a comprehensive description" cols="30" rows="10"></textarea>
+                                </fieldset>
+                            </div>   
+                        </section>
+                        <section id="prompt-author-info">
+                            <p class="grey">Created: now :P</p>
+                            <a class="white-a profile-a" href="../profile?id=<?php echo $_SESSION['userId']; ?>">
+                                <span class="grey">By:</span>
+                                <figure style="background-image: url(../<?php echo $_SESSION['profile-pic'] ?>);"></figure>
+                                <span><?php echo htmlspecialchars($_SESSION['username']); ?></span>
+                            </a>
+                        </section>
+                        <section id="prompt-section">
+                            <h2>The prompt:</h2>
+                            <textarea class="grey-textarea" name="prompt" id="prompt" cols="30" rows="5"></textarea>
+                            <script>
+                                document.querySelector("#prompt").addEventListener('keyup', e => {
+                                    const wordCount = document.querySelector("#prompt").value.match(/\S+/g) === null ? 0 : document.querySelector("#prompt").value.match(/\S+/g).length
+                                    
+                                    document.querySelector("#word-count").innerHTML = wordCount
+                                })
+                            </script>
+                        </section>
                     </div>
-                    <section>
+                    <section id="example-image-container">
+                        <input data-type="image" type="file" name="prompt-example-image1" id="prompt-example-image1" accept=".jpg, .jpeg, .png, .webp" class="hidden"> 
                         <label for="prompt-example-image1" class="add-prompt-example-image"></label>
-                        <input type="file" name="prompt-example-image1" id="prompt-example-image1" accept=".jpg, .jpeg, .png, .webp" class="hidden"> 
-                        <label for="prompt-example-image1" class="add-prompt-example-image add-prompt-another-image"></label>
-                        <input type="file" name="prompt-example-image1" id="prompt-example-image1" accept=".jpg, .jpeg, .png, .webp" class="hidden">
+                        <input data-type="image" type="file" name="prompt-example-image2" id="prompt-example-image2" accept=".jpg, .jpeg, .png, .webp" class="hidden">
+                        <label for="prompt-example-image2" class="add-prompt-example-image"></label>
                     </section>
                 </div>
             </div>
-        </div>
+        </form>
     </main>
 </body>
 </html>
