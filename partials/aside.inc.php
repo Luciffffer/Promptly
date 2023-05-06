8d58f3339076dd629ed77404db6753947c057555
Lucifer <tim.noelmans@outlook.com>
2023-05-06 21:21:39 +0200

    feat(aside_nav): aside navigation V1 complete

--- partials/aside.inc.php
+++ partials/aside.inc.php
@@ -1,28 +1,48 @@
-<aside>
-    <a class="button primary-btn-white" href="<?php echo __ROOT__; ?>tools/add-prompt">
-        <img src="<?php echo __ROOT__; ?>/assets/images/site/plus-icon.svg" alt="Icon of a plus">
-        Add a prompt
-    </a>
-    <h2 class="titles">Browse</h2>
-    <ul class="nav-ul">
-        <li><a href="#">Hot</a></li>
-        <li><a href="#">New</a></li>
-        <li><a href="#">Top</a></li>
-    </ul>
-    <h2 class="titles">Models</h2>
-    <ul class="nav-ul">
-        <li><a href="#">ChatGPT</a></li>
-        <li><a href="#">MidJourney</a></li>
-        <li><a href="#">Stable Diffusion</a></li>
-        <li><a href="#">Open Diffusion</a></li>
-        <li><a href="#">DALL-E</a></li>
-    </ul>
-    <h2 class="titles">Your Library</h2>
-    <ul class="nav-ul">
-        <li><a href="#">Liked</a></li>
-        <li><a href="#">Bought</a></li>
-        <li><a href="#">Stable Diffusion</a></li>
-        <li><a href="#">DALL-E</a></li>
-    </ul>
+<?php 
+
+include_once($_SERVER['DOCUMENT_ROOT'] . __ROOT__ . 'classes/Prompt.php');
+
+$models = Prompt::getAllModels();
+
+?><aside>
+    <nav aria-label="Platform/tab navigator" id="aside-nav">
+        <a class="button primary-btn-white" href="<?php echo __ROOT__; ?>tools/add-prompt">
+            <img src="<?php echo __ROOT__; ?>/assets/images/site/plus-icon.svg" alt="Icon of a plus">
+            <span>Add Prompt</span>
+        </a>
+        <div>
+            <a href="<?php echo __ROOT__; ?>discover" class="aside-title">
+                <img src="<?php echo __ROOT__ ;?>/assets/images/site/home-icon.svg" alt="Home icon">
+                <h2>Browse</h2>
+            </a>
+            <ul class="aside-ul">
+                <li><a href="<?php echo __ROOT__; ?>discover">Discover</a></li>
+                <li><a href="#">Popular</a></li>
+                <li><a href="#">New</a></li>
+            </ul>
+        </div>
+        <div id="aside-models">
+            <div class="aside-title">
+                <img src="<?php echo __ROOT__ ;?>/assets/images/site/robot-icon.svg" alt="Model icon">
+                <h2>Models</h2>
+            </div>
+            <ul class="aside-ul">
+                <?php foreach ($models as $model) : ?>
+                    <li><a href="#"><?php echo $model['name']; ?></a></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+        <div>
+            <a href="#" class="aside-title">
+                <img src="<?php echo __ROOT__ ;?>/assets/images/site/library-icon.svg" alt="Library icon">
+                <h2>Library</h2>
+            </a>
+            <ul class="aside-ul">
+                <li><a href="#">Bought</a></li>
+                <li><a href="#">Liked</a></li>
+                <li><a href="#">Your Prompts</a></li>
+            </ul>
+        </div>
+    </nav>
 </aside>
 <div id="aside-placeholder"></div>
