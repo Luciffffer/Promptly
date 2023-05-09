c4fb28980b76353e03f9b4e4b0a1e7c3c7114b34
zegher <zegherb@hotmail.com>
2023-05-09 11:32:14 +0200

    feat: add moderation tool function

--- classes/Moderator.php
+++ classes/Moderator.php
@@ -4,5 +4,14 @@ require_once(__DIR__ . "/User.php");
 
 class Moderator extends User
 {
-
+    public static function isMod($id){
+        $PDO = Database::getInstance();
+        $statement = $PDO->prepare("SELECT * FROM `users` WHERE isMod = 1 AND id = :id");
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+        
+        if($statement->rowCount() > 0){
+            return $_SESSION['isMod'] = true;
+        }
+    }
 }
