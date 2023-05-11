b3c13ba245a098f50ba6d4ffa655ac73aae17b0b
WillTDP <williamhazevoets@gmail.com>
2023-05-11 09:53:07 +0200

    FEAT: started the like system

--- classes/Like.php
+++ classes/Like.php
@@ -1,6 +1,32 @@
 <?php
+require_once(__DIR__ . "/Database.php");
 
 class like
 {
-    
+    public function addLike($promptId, $userId)
+    {
+        $pdo = Database::getInstance();
+
+        try {
+            $stmt = $pdo->prepare("INSERT INTO likes (prompt_id, user_id) VALUES (:prompt_id, :user_id)");
+            $stmt->bindParam(':prompt_id', $promptId, PDO::PARAM_INT);
+            $stmt->bindParam(':user_id', $userId, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            return true;
+        } catch (PDOException $e) {
+            echo "Error adding like: " . $e->getMessage();
+            return false;
+        }
+    }
+}
+
+$likes = new like();
+$promptId = 1;
+$userId = 1;
+
+if ($likesManager->addLike($promptId, $userId)) {
+    echo "Like added successfully!";
+} else {
+    echo "Failed to add like.";
 }
--- prompt.php
+++ prompt.php
@@ -2,6 +2,7 @@
 
 include_once(__DIR__ . '/classes/Prompt.php');
 include_once(__DIR__ . '/classes/User.php');
+include_once(__DIR__ . '/classes/Like.php');
 
 session_start();
 
